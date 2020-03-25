c982dbc8fbaf1714d1664ec8111624ac4c4e4df5
Pontus Pihlgren <pontus@update.uu.se>
2020-03-25 23:31:24 +0100

    Move controller code and state to functions

--- rx8.c
+++ rx8.c
@@ -29,14 +29,8 @@ short bit_mode = 1; // Bit Mode.  0 = 8-bit, !0 = 12-bit
 short maintenance_mode = 0; // Maintenance mode. 0 = off, !0 = on
 short intr_enabled = 0; // RX8E may generate interrupts
 
-// Controller (RX01) bits and registers, one set for each drive.
-short RXES[2] = {0}; // RX Error and Status bits
-short track[2] = {0}; // Current track, always between 0 and 0114
-short sector[2] = {0}; // Current sector, always between 1 and 032
-short sector_buffer[2][64] = {0}; // Buffer for read and write
-short current_function = -1; // Function being performed, may take
-                             // several host instructions
-short current_drive = 0; // Drive the current function is performed on-
+static void controller_LCD(short ac);
+static short controller_XDR();
 
 void rx8_process(short mb)
 {
@@ -45,12 +39,11 @@ void rx8_process(short mb)
     break;
   case RX_LCD: // Load Command (clear AC)
     rx_ir = ac;
-    current_function = (ac & RX_FUNC_MASK) >> 1;
-    current_drive = (ac & RX_DRVSEL_MASK) ? 1 : 0;
-    bit_mode = (ac & RX_MODE_MASK);
-    maintenance_mode = (ac & RX_MAINT_MASK) ? 1 : 0;
+    controller_LCD(rx_ir);
+    bit_mode = (rx_ir & RX_MODE_MASK);
+    maintenance_mode = (rx_ir & RX_MAINT_MASK) ? 1 : 0;
     if( maintenance_mode ){
-      rx_tr = tr_df = rx_ef = maintenance_mode; // Setting maint mode raises all flags
+      rx_tr = tr_df = rx_ef = 1; // Setting maint mode raises all flags
     }
     ac = (ac & LINK_MASK);
     break;
@@ -87,4 +80,24 @@ void rx8_process(short mb)
     printf("illegal IOT instruction. Device: %o. Operation: %o - RX8\n", (mb & DEV_MASK) >> 3, mb & IOT_OP_MASK );
     break;
   }
+}
+
+// Controller (RX01) bits and registers, one set for each drive.
+short RXES[2] = {0}; // RX Error and Status bits
+short track[2] = {0}; // Current track, always between 0 and 0114
+short sector[2] = {0}; // Current sector, always between 1 and 032
+short sector_buffer[2][64] = {0}; // Buffer for read and write
+short current_function = -1; // Function being performed, may take
+                             // several host instructions
+short current_drive = 0; // Drive the current function is performed on-
+
+void controller_LCD(short ir)
+{
+  current_function = (ir & RX_FUNC_MASK) >> 1;
+  current_drive = (ir & RX_DRVSEL_MASK) ? 1 : 0;
+}
+
+short controller_XDR()
+{
+  return 0;
 }
